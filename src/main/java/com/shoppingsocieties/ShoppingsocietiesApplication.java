2e2b8208d5990e0a644672c63f8c1e8784e023e7
shashimal <shasimald@gmail.com>
2018-10-17 17:56:36 +0800

    Created the Shopping Societies project folder

---
+++ src/main/java/com/shoppingsocieties/ShoppingsocietiesApplication.java
@@ -0,0 +1,12 @@
+package com.shoppingsocieties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingsocietiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingsocietiesApplication.class, args);
+    }
+}
