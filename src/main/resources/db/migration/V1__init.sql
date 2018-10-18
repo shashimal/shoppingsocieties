ec2b3fe306e4ec64d6458e30659317272b3f468d
shashimal <shasimald@gmail.com>
2018-10-18 17:49:57 +0800

    Changed user and wallet table schema with data population script

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,14 +1,16 @@
-CREATE TABLE `user` (
-  `user_id` BIGINT(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
+CREATE TABLE `wallet` (
+  `id` BIGINT(11) unsigned NOT NULL AUTO_INCREMENT,
+  `currency` VARCHAR (6) NOT NULL,
+  `balance` DOUBLE(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `wallet` (
-  `wallet_id` BIGINT(11) unsigned NOT NULL AUTO_INCREMENT,
-  `currency_code` VARCHAR (6) NOT NULL,
-  `balance` DOUBLE DEFAULT NULL,
-  `user_id` BIGINT(11) unsigned NOT NULL,
-  PRIMARY KEY (`wallet_id`),
-  CONSTRAINT `fk_wallet_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `user` (
+  `id` BIGINT(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) DEFAULT NULL,
+  `wallet_id` BIGINT(11) unsigned DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   UNIQUE KEY unique_user_id (wallet_id),
+   CONSTRAINT `fk_user_wallet` FOREIGN KEY (`wallet_id`) REFERENCES `wallet` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
--- src/main/resources/db/migration/V2__initial_data.sql
+++ src/main/resources/db/migration/V2__initial_data.sql
@@ -1,10 +1,11 @@
-INSERT INTO user (user_id,name) values (1,'Shopping Societies');
-INSERT INTO user (user_id,name) values (2,'Duleendra Shashimal');
-INSERT INTO user (user_id,name) values (3,'John Mark');
-INSERT INTO user (user_id,name) values (4,'Michel Clerk ');
-INSERT INTO user (user_id,name) values (5,'Simon Silva');
-INSERT INTO user (user_id,name) values (6,'Stanley See');
 
 -- Default data for wallet table
-INSERT INTO wallet (wallet_id, currency_code, balance,user_id) values (1,'SG',0,1);
-INSERT INTO wallet (wallet_id, currency_code, balance,user_id) values (2,'SG',0,2);
+INSERT INTO wallet (id, currency, balance) values (1,'SGD',5500.50);
+INSERT INTO wallet (id, currency, balance) values (2,'SGD',0);
+
+INSERT INTO user (id,name,wallet_id) values (1,'Shopping Societies',1);
+INSERT INTO user (id,name,wallet_id) values (2,'Duleendra Shashimal',2 );
+INSERT INTO user (id,name,wallet_id) values (3,'John Mark',null);
+INSERT INTO user (id,name,wallet_id) values (4,'Michel Clerk',null);
+INSERT INTO user (id,name,wallet_id) values (5,'Simon Silva',null);
+INSERT INTO user (id,name,wallet_id) values (6,'Stanley See',null);
