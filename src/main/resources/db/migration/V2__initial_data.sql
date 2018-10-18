982031c39856e631cd5d86b576b241cfcf79b0b0
shashimal <shasimald@gmail.com>
2018-10-18 11:55:04 +0800

    Flyway script for user and wallet table's creation

---
+++ src/main/resources/db/migration/V2__initial_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO user (user_id,name) values (1,'Shopping Societies');
+INSERT INTO user (user_id,name) values (2,'Duleendra Shashimal');
+INSERT INTO user (user_id,name) values (3,'John Mark');
+INSERT INTO user (user_id,name) values (4,'Michel Clerk ');
+INSERT INTO user (user_id,name) values (5,'Simon Silva');
+INSERT INTO user (user_id,name) values (6,'Stanley See');
+
+-- Default data for wallet table
+INSERT INTO wallet (wallet_id, currency_code, balance,user_id) values (1,'SG',0,1);
+INSERT INTO wallet (wallet_id, currency_code, balance,user_id) values (2,'SG',0,2);
