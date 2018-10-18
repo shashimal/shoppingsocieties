aef8e386bee5fcb9f5eeb35225fd62797ddd531c
shashimal <shasimald@gmail.com>
2018-10-18 20:59:43 +0800

    Common string constant's class

--- src/main/java/com/shoppingsocieties/common/StringConstant.java
+++ src/main/java/com/shoppingsocieties/common/StringConstant.java
@@ -2,7 +2,7 @@ package com.shoppingsocieties.common;
 
 /***
  * Common string constants
- * 
+ *
  */
 public class StringConstant {
     public static final String ENTITY_NOT_FOUND="Entity not found";
