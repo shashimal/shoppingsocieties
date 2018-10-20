d18caf3178efa86374daa57ec619731dff1810cc
shashimal <shasimald@gmail.com>
2018-10-21 00:35:52 +0800

    Added the sale's response class

---
+++ src/main/java/com/shoppingsocieties/api/response/SaleResponse.java
@@ -0,0 +1,18 @@
+package com.shoppingsocieties.api.response;
+
+import com.shoppingsocieties.dto.SalesDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleResponse {
+
+    List<SalesDTO> sales;
+}
