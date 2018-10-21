9a31230a98dfd928974dd8c25a3a9220cd86120e
shashimal <shasimald@gmail.com>
2018-10-21 13:37:50 +0800

    Added new wallet entry for the data population sql

--- src/main/resources/db/migration/V2__initial_data_wallet.sql
+++ src/main/resources/db/migration/V2__initial_data_wallet.sql
@@ -2,3 +2,5 @@
 INSERT INTO wallet (id, currency, balance) values (1,'SGD',10000.00);
 INSERT INTO wallet (id, currency, balance) values (2,'SGD',4000.00);
 INSERT INTO wallet (id, currency, balance) values (3,'SGD',150.00);
+INSERT INTO wallet (id, currency, balance) values (4,'SGD',00.00);
+
